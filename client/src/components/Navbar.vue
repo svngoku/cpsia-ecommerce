906fa546c47241bfff8251c101fb953099125deb
svngoku <marymarty104@gmail.com>
2020-06-24 04:45:29 +0200

    adding vuejs client

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+         <!-- <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+            <div class="px-3 py-2">
+                <p>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </p>
+                <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+            </div>
+        </b-sidebar> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">RADIAN</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to="/">Acceuil</router-link>
+                        <span class="sr-only">(current)</span>
+                    </li>
+                    <li class="nav-item">
+                       
+                    </li>
+                    <li class="nav-item">
+                         <router-link class="nav-link" to="/about">Produits</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    <!-- <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button> -->
+                </ul>
+            </div>
+            </nav>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Navbar"
+};
+</script>
---
+++ client/src/views/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <h1>Produits page</h1>
+    <div class="row">
+      <div class="col-sm-6" v-for="(produit,i) in produits" :key="i">
+      <br />
+        <div class="card">
+          <img class="responsive-image" v-bind:src='produit.img_produit' height="400" width="auto" />
+          <div class="card-body">
+            <h5 class="card-title">{{ produit.titre }}</h5>
+            <p class="card-text">{{ produit.description }}</p>
+            <p class="card-text">Prix: {{ produit.prix  }} €</p>
+            <a href="#" class="btn btn-warning">🛒</a>
+            <br />
+            <br />
+             <b-btn v-b-toggle.collapse1 variant="warning">Toggle Collapse</b-btn>
+          </div>
+        </div>
+      </div>
+    </div>       
+  </div>
+</template>
+
+
+<script>
+import axios from "axios"
+export default {
+  props: ["produit"],
+  data() {
+    return {
+      produits: []
+    }
+  },
+  async created(){
+    await axios
+      .get('http://localhost:8000/api/services/produits/read.php')
+      .then(response => (this.produits = response.data.produits))
+  },
+  // methods: {
+  //   showByID: function(id) {
+  //     id = this.articles.id
+  //     axios.get(`http://localhost:8000/api/services/produits/read.php/${id}`).then(response => (this.produit = response.data))
+  //   }
+  // },
+  
+}
+</script>
